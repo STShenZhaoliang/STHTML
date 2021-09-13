ff56898735549a1d5d650103d34de02cee400fae
szl <STShenZhaoliang@users.noreply.github.com>
2021-09-13 15:07:00 +0800

    vue-day04

--- 05-Vue/02-vue/day04/mybook/router.js
+++ 05-Vue/02-vue/day04/mybook/router.js
@@ -6,17 +6,22 @@ const router = express.Router();
 const service = require('./service.js');
 
 // 查询图书列表
-router.get('/books',service.getAllBooks);
+router.get('/list', service.getAllBooks);
 // 添加图书(提交表单)
-router.post('/books',service.addBook);
+router.post('/add', service.addBook);
 // 跳转到编辑图书信息页面
-router.get('/books/:id',service.toEditBook);
+router.get('/books/:id', service.toEditBook);
 // router.get('/toEditBook',service.toEditBook);
 // 编辑图书提交表单
-router.put('/books/:id',service.editBook);
+router.put('/list/:id', service.editBook);
 // 删除图书信息
-router.delete('/books/:id',service.deleteBook);
+router.delete('/list/:id', service.deleteBook);
+router.delete('/deleteall', service.deleteAllComplete);
 // 验证图书名称是否存在
-router.get('/books/book/:name',service.checkName);
+// router.get('/books/book/:name', service.checkName);
+
+// 实现全选功能
+router.get('/all/:val', service.allchecked);
+
 
 module.exports = router;
