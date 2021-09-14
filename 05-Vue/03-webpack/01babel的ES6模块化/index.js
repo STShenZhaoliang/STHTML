f7f6205c5c3fbeb1eaafb274e84ba764e0ec85e0
szl <STShenZhaoliang@users.noreply.github.com>
2021-09-14 09:49:39 +0800

    babel

---
+++ 05-Vue/03-webpack/01babel的ES6模块化/index.js
@@ -0,0 +1,13 @@
+import m11, { s1, s2, say } from "./m1";
+
+console.log(m11.a);
+console.log(m11.b);
+console.log(m11.show());
+console.log(m11.show);
+console.log(s1);
+console.log(s2);
+// console.log(s3);
+console.log(say);
+
+import m2 from './m2'
+console.log(m2)
---
+++ 05-Vue/03-webpack/01babel的ES6模块化/m1.js
@@ -0,0 +1,17 @@
+let a = 10
+let b = 20
+
+function show() {
+    console.log("show");
+    return "show info"
+}
+export default {
+    a,
+    b,
+    show
+}
+
+export let s1 = "aaa"
+export let s2 = "bbb"
+export let s3 = "ccc"
+export function say() {}
