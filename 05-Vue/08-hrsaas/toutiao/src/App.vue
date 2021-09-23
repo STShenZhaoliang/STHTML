02f93cb5eedcf088cafd5e36340c4902103b448a
沈天 <STShenZhaoliang@users.noreply.github.com>
2021-09-23 15:36:29 +0800

    vuex-toutiao

---
+++ 05-Vue/08-hrsaas/toutiao/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <catagtory/>
+    <new-list/>
+  </div>
+</template>
+
+<script>
+import Catagtory from './components/catagtory.vue'
+import NewList from './components/new-list.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Catagtory,
+    NewList
+  }
+}
+</script>
