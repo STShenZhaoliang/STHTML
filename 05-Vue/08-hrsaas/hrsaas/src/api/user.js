d225d105a2948dc656fe52850f4662c672bb6181
沈天 <13865738+szlsay@users.noreply.github.com>
2021-09-25 16:53:53 +0800

    hrsaas-login

--- 05-Vue/08-hrsaas/hrsaas/src/api/user.js
+++ 05-Vue/08-hrsaas/hrsaas/src/api/user.js
@@ -1,7 +1,34 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
-export function login(data) {}
+/** *
+ * 登陆接口封装
+ * **/
+export function login(data) {
+// 返回一个promise对象
+  return request({
+    url: '/sys/login',
+    method: 'post',
+    data
+  })
+}
+/**
+获取用户资料接口
+**/
+export function getUserInfo() {
+  return request({
+    url: '/sys/profile',
+    method: 'post'
+  })
+}
+/** **
+ *
+ * 根据用户id获取用户的详情
+ * ****/
+export function getUserDetailById(id) {
+  return request({
+    url: `/sys/user/${id}`
+  })
+}
+export function logout() {
 
-export function getInfo(token) {}
-
-export function logout() {}
+}
